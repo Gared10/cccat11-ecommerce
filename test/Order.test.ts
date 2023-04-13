5126c9439cbad555b7d653425174f514c9d35a99
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-04-12 22:06:29 -0300

    feat: refactoring test to use main order route

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -1,10 +1,16 @@
-import Order from "../src/Order"
+import axios from "axios";
 
-test('Should create an order with 3 items and calculate total amount', () => {
-  let order: Order = new Order();
-  order.addOrderItem('Gillette', 2, 10);
-  order.addOrderItem('Always', 5, 15);
-  order.addOrderItem('Queijo', 1, 25);
-  expect(order.getItems()?.length).toBe(3);
-  expect(order.getTotal()).toBe(120);
+test('Should create an order with 3 items and calculate total amount', async function () {
+  const order = {
+    items: [
+      {"description": 'Gillette', "quantity": 2, "price": 10},
+      {"description": 'Always', "quantity": 5, "price": 15},
+      {"description": 'Queijo', "quantity": 1, "price": 25}
+    ]
+  }
+
+  const data = await axios.post("http://localhost:3000/order", order)
+
+  expect(data.data.items).toBe(3);
+  expect(data.data.total).toBe(120);
 })
