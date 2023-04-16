dd299d79f92bd1ea06340b63d0c4f1139b692e31
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-04-15 22:32:41 -0300

    feat: adding test with cpf not informed in request

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -52,3 +52,20 @@ test('Should alert that the cpf is invalid and not create any order', async func
   expect(data.data.total).toBe(0);
   expect(data.data.message).toBe('CPF inválido!');
 })
+
+test('Should alert that the cpf is invalid and not create any order because cpf field is not filled in request data', async function () {
+  const order = {
+    items: [
+      {"description": 'Gillette', "quantity": 2, "price": 10},
+      {"description": 'Always', "quantity": 5, "price": 15},
+      {"description": 'Queijo', "quantity": 1, "price": 25}
+    ],
+    coupon: 5
+  }
+
+  const data = await axios.post("http://localhost:3000/order", order)
+
+  expect(data.data.items).toBe(0);
+  expect(data.data.total).toBe(0);
+  expect(data.data.message).toBe('CPF inválido!');
+})
