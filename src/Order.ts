a7a7431875f14d0913f189680113025e238a5b84
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-04-24 21:41:42 -0300

    refactor: removing wrong code

--- src/Order.ts
+++ src/Order.ts
@@ -30,33 +30,5 @@ export default class Order {
     return this.cpf
   }
 
-  isValidCpf(): boolean{
-    let digit: number = 0;
-    let firstVerifierSum: number = 0;
-    let secondVerifierSum: number = 0;
-    let firstVerifierDigit: string = '';
-    let secondVerifierDigit: string = '';
-    let mod: number = 0;
-    let validateCPF: string = '';
-
-    if (this.cpf.length < 11) return false;
-    for(digit; digit<this.cpf.length; digit++){
-      firstVerifierSum += (Number(this.cpf[digit])*(11-(digit+1)))
-      if(digit+1 === 9){
-        mod = firstVerifierSum % 11
-        firstVerifierDigit = mod >= 2 ? String(11-mod) : '0'
-        mod = 0
-      }
-      secondVerifierSum += ((digit+1 !== 10 ? Number(this.cpf[digit]) : Number(firstVerifierDigit)) * (11-(digit)))
-      if(digit+1 === 10){
-        mod = secondVerifierSum % 11
-        secondVerifierDigit = mod >= 2 ? String(11-mod) : '0'
-        mod = 0
-      }
-    }
-    validateCPF = this.cpf.substring(0,9) + firstVerifierDigit + secondVerifierDigit
-    return validateCPF === this.cpf ? true : false
-  }
-
 
 }
