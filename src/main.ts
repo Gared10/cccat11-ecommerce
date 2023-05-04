fa44ba3681a64d1f22d9d33e0c02a04c74c3b5fb
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-05-03 22:25:57 -0300

    feat: adding postgre to the project

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import express, { Request, Response } from "express";
 import OrderItem from "./OrderItem";
 import Order from "./Order";
 import { validate } from "./validateCPF";
+import config from "./config";
+import pgp from "pg-promise";
 
 const app = express()
 
 app.use(express.json())
 
 app.post("/order", async function (request: Request, response: Response) {
+
   const result = {
     total: 0,
     items: 0,
@@ -15,22 +18,30 @@ app.post("/order", async function (request: Request, response: Response) {
   }
   let items: OrderItem[]
   let order: Order
-  if(!validate(request.body.cpf)) return response.json({message: 'Invalid cpf', total: result.total, items: result.items});
-  if(request.body.items){
+
+  if (!validate(request.body.cpf)) return response.json({ message: 'Invalid cpf', total: result.total, items: result.items });
+
+  const connection = pgp()("postgres://postgres:postgres123@localhost:5432/app")
+  if (request.body.items) {
     order = new Order(request.body.cpf)
-    
-    request.body.items.map((item: any) => {
-      order.addOrderItem(item.description, item.quantity, item.price);
-    })
+    for (const item of request.body.items) {
+      const [productData] = await connection.query("select description, price from ecommerce.product where id_product = $1", [item.idProduct]);
+      order.addOrderItem(productData.description, item.quantity, productData.price);
+    }
     result.total = order.getTotal();
     result.items = order.getItems().length;
-    if(request.body.coupon){
-      result.total -= order.getTotal() * (request.body.coupon/100);
+    if (request.body.coupon) {
+      const [couponData] = await connection.query('select percentage from ecommerce.coupon where code = $1', [request.body.coupon]);
+      const percentage: number = parseFloat(couponData.percentage);
+      result.total -= order.getTotal() * (percentage / 100);
     }
   }
+
   return response.json(result);
 })
+
 
 app.listen(3000, () => {
   console.log('Server running on port 3000!')
 })
+
