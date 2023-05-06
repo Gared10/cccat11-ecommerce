69bb5473c48b43a766b6170f724e7b766b32d999
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-05-06 11:35:23 -0300

    feat: calculate minimal fare with negative measure

--- test/calculateFare.test.ts
+++ test/calculateFare.test.ts
@@ -25,3 +25,16 @@ test('Should calculate minimal fare price', function () {
 
   expect(fare).toBe(10);
 })
+
+test('Should calculate minimal fare price', function () {
+  const product = {
+    price: 1000,
+    height: -10,
+    width: 15,
+    product_length: 20,
+    weight: 1
+  }
+  const fare: number = calculateFare(product.height, product.width, product.product_length, product.weight)
+
+  expect(fare).toBe(10);
+})
