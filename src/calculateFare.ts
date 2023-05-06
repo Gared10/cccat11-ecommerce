154f801fe56c8ebfeed7f3d2f155b2122a15ddf3
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-05-06 11:32:30 -0300

    feat: calculate fare functionality

---
+++ src/calculateFare.ts
@@ -0,0 +1,7 @@
+export function calculateFare(height: number, width: number, product_length: number, weight: number): number {
+  const volume: number = height * width * product_length;
+  const density: number = weight / volume;
+  const fare: number = 1000 * volume * (density / 100);
+
+  return fare >= 10 ? fare : 10;
+}
---
+++ test/calculateFare.test.ts
@@ -0,0 +1,27 @@
+import { calculateFare } from "../src/calculateFare"
+
+test('Should calculate products fare', function () {
+  const product = {
+    price: 5000,
+    height: 10,
+    width: 30,
+    product_length: 100,
+    weight: 3
+  }
+  const fare: number = calculateFare(product.height, product.width, product.product_length, product.weight)
+
+  expect(fare).toBe(30);
+})
+
+test('Should calculate minimal fare price', function () {
+  const product = {
+    price: 1000,
+    height: 10,
+    width: 15,
+    product_length: 20,
+    weight: 1
+  }
+  const fare: number = calculateFare(product.height, product.width, product.product_length, product.weight)
+
+  expect(fare).toBe(10);
+})
