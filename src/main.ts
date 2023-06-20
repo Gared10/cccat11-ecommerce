b56331b6fc20a71629b4864c743977c9fce598c2
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-06-19 23:18:54 -0300

    refactor: utilizando novas classes feitas para desacoplar utilização de servidor http com todos os use cases utilizando factories criados previamente

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,16 @@
-import express, { Request, Response } from "express";
-import Checkout from "./Checkout";
-import ListOrders from "./ListOrders";
 import DatabaseRepositoryFactory from "./DatabaseRepositoryFactory";
 import PgPromiseAdapter from "./PgPromiseAdapter";
+import ExpressAdapter from "./ExpressAdapter";
+import HttpController from "./HttpController";
+import HttpUseCaseFactory from "./HttpUseCaseFactory";
 
-const app = express()
-
-app.use(express.json())
+const port = 3000;
 const connection = new PgPromiseAdapter()
 connection.connect();
 const repositoryFactory = new DatabaseRepositoryFactory(connection);
-const checkout = new Checkout(repositoryFactory);
-const listOrders = new ListOrders(repositoryFactory);
+const useCaseFactory = new HttpUseCaseFactory(repositoryFactory);
+const httpServer = new ExpressAdapter();
+new HttpController(httpServer, useCaseFactory);
 
-app.post("/checkout", async function (request: Request, response: Response) {
-  try {
-    const output = await checkout.execute(request.body);
-    response.json(output);
-  } catch (error: any) {
-    response.status(422).json({
-      message: error.message
-    });
-  }
-
-})
-
-app.get("/orders", async function (request: Request, response: Response) {
-  try {
-    const output = await listOrders.execute();
-    response.json(output);
-  } catch (error: any) {
-    response.status(422).json({
-      message: error.message
-    });
-  }
-
-})
-
-
-app.listen(3000, () => {
-  console.log('Server running on port 3000!')
-})
+httpServer.listen(port, `Server running on port ${port}!`);
 
