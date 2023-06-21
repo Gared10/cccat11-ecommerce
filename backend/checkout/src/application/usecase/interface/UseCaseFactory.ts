019b6c27529c684ebacd78d9715ca712140e1835
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-06-21 20:45:04 -0300

    fear: adicionando factories dos dois novos casos de uso do catálogo

--- backend/checkout/src/application/usecase/interface/UseCaseFactory.ts
+++ backend/checkout/src/application/usecase/interface/UseCaseFactory.ts
@@ -2,9 +2,13 @@ import Checkout from "../Checkout";
 import ListOrders from "../ListOrders";
 import RepositoryFactory from "./RepositoryFactory";
 import SimulateFare from "../SimulateFare";
+import GetProduct from "../GetProduct";
+import ListProducts from "../ListProducts";
 
 export default interface UseCaseFactory {
   createCheckout(): Checkout;
   createListOrders(): ListOrders;
   createSimulateFare(): SimulateFare;
+  createGetProduct(): GetProduct;
+  createListProducts(): ListProducts;
 }
