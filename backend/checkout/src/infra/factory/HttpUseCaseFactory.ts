63f3a82d6fade724dd1913638bee8d54d3680589
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-06-21 20:45:45 -0300

    feat: implementando novos métodos das interfaces de factory de use cases e do repositório de produtos

--- backend/checkout/src/infra/factory/HttpUseCaseFactory.ts
+++ backend/checkout/src/infra/factory/HttpUseCaseFactory.ts
@@ -3,12 +3,19 @@ import ListOrders from "../../application/usecase/ListOrders";
 import RepositoryFactory from "../../application/usecase/interface/RepositoryFactory";
 import SimulateFare from "../../application/usecase/SimulateFare";
 import UseCaseFactory from "../../application/usecase/interface/UseCaseFactory";
+import GetProduct from "../../application/usecase/GetProduct";
+import ListProducts from "../../application/usecase/ListProducts";
 
 export default class HttpUseCaseFactory implements UseCaseFactory {
 
   constructor(readonly repositoryFactory: RepositoryFactory) {
   }
-
+  createGetProduct(): GetProduct {
+    return new GetProduct(this.repositoryFactory);
+  }
+  createListProducts(): ListProducts {
+    return new ListProducts(this.repositoryFactory);
+  }
   createCheckout(): Checkout {
     return new Checkout(this.repositoryFactory);
   }
