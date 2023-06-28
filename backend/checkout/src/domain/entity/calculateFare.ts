4cb9a5312c65bf816028cccbc48e11101d96a4a3
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-06-27 23:00:21 -0300

    refactor: removendo entidades não utilizadas no bounded context de checkout

--- backend/checkout/src/domain/entity/calculateFare.ts
+++
@@ -1,9 +0,0 @@
-import Product from "./Product";
-
-export function calculateFare(product: Product, distance: number): number {
-  const volume: number = product.getVolume();
-  const density: number = product.getDensity();
-  const fare: number = distance * volume * (density / 100);
-
-  return fare >= 10 ? Math.round(fare) : 10;
-}
