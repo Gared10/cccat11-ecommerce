7802bd5219d73b255957ae3ece856ced27f4733f
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-06-27 23:03:21 -0300

    feat: adicionandoscript de criação de tabela de usuários

---
+++ backend/auth/create.sql
@@ -0,0 +1,54 @@
+create schema ecommerce;
+
+create table ecommerce.product (
+	  id_product integer,
+	  description text,
+	  price numeric,
+  	height numeric,
+  	width numeric,
+  	product_length numeric,
+  	weight numeric
+);
+
+insert into ecommerce.product (id_product, description, price, height, width, product_length, weight) values (1, 'Camera', 1000, 10, 15, 20, 1);
+insert into ecommerce.product (id_product, description, price, height, width, product_length, weight) values (2, 'Guitarra', 5000, 10, 30, 100, 3);
+insert into ecommerce.product (id_product, description, price, height, width, product_length, weight) values (3, 'Geladeira', 8000, 50, 100, 200, 40);
+
+create table ecommerce.coupon (
+	  code text,
+	  percentage numeric,
+  	expiration_date timestamp with time zone
+);
+
+insert into ecommerce.coupon (code, percentage, expiration_date) values ('VALE20', 20, '2023-05-01 23:59:59');
+insert into ecommerce.coupon (code, percentage, expiration_date) values ('VALE20_2', 20, '2022-05-01 23:59:59');
+
+create table ecommerce.order_header (
+	id_order text,
+	code text,
+	cpf text,
+	fromcep text,
+	tocep text
+)
+
+create table ecommerce.order_items (
+	id_product integer,
+	id_order text,
+	quantity numeric,
+	price numeric
+)
+
+create table ecommerce.location(
+	CEP text,
+	latitude numeric,
+	longitude numeric
+)
+
+insert into ecommerce.location(CEP, latitude, longitude) values("88015600", -27.5906685, -48.5605664)
+insert into ecommerce.location(CEP, latitude, longitude) values("22030060", -9.610394, -35.725652)
+
+create table ecommerce.user(
+	email text,
+	password text,
+	salt text
+)
