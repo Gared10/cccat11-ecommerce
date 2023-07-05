9e42a800b584e46305b9974ed5ff79f313732250
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-07-04 21:26:35 -0300

    feat: criando rota de verificação de token na api

--- backend/auth/src/infra/http/HttpController.ts
+++ backend/auth/src/infra/http/HttpController.ts
@@ -3,15 +3,11 @@ import UseCaseFactory from "../../application/usecase/interface/UseCaseFactory";
 
 export default class HttpController {
 
-
   constructor(readonly httpServer: HttpServer, readonly useCaseFactory: UseCaseFactory) {
 
-    httpServer.on("get", "/products/:idProduct", async (body: any, params: any, headers: any) => {
-      const output = {};
-      return output;
-    })
-    httpServer.on("get", "/products", async (body: any, params: any, headers: any) => {
-      const output = {};
+    httpServer.on("post", "/verify", async (body: any, params: any, headers: any) => {
+      const verify = useCaseFactory.createVerify();
+      const output = await verify.execute(body.token);
       return output;
     })
   }
