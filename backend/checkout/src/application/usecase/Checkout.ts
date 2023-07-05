7dd681dd08f624a19ca65e196ac3cc4d28524a0e
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-07-04 23:45:45 -0300

    refactor/fix/feat: ajustando use cases para que os testes passassem, tentando generalizar funcionamento independente entre bounded contexts

--- backend/checkout/src/application/usecase/Checkout.ts
+++ backend/checkout/src/application/usecase/Checkout.ts
@@ -4,10 +4,10 @@ import Order from "../../domain/entity/Order";
 import { validate } from "../../domain/entity/validateCPF";
 import GatewayFactory from "../factory/GatewayFactory";
 import RepositoryFactory from "../factory/RepositoryFactory";
+import AuthGateway from "../gateway/AuthGateway";
 import CatalogGateway from "../gateway/CatalogGateway";
 import FareGateway from "../gateway/FareGateway";
 import CouponRepository from "../repository/CouponRespository";
-import LocationRepository from "../repository/LocationRepository";
 import OrderRepository from "../repository/OrderRepository";
 import ProductRepository from "../repository/ProductRepository";
 
@@ -15,9 +15,9 @@ export default class Checkout {
   orderRepository: OrderRepository;
   productRepository: ProductRepository;
   couponRepository: CouponRepository;
-  locationRepository: LocationRepository;
   catalogGateway: CatalogGateway;
   fareGateway: FareGateway;
+  authGateway: AuthGateway;
 
   constructor(
     repositoryFactory: RepositoryFactory,
@@ -26,19 +26,21 @@ export default class Checkout {
     this.orderRepository = repositoryFactory.createOrderRepository();
     this.productRepository = repositoryFactory.createProductRepository();
     this.couponRepository = repositoryFactory.createCouponRepository();
-    this.locationRepository = repositoryFactory.createLocationRepository();
     this.catalogGateway = gatewayFactory.createCatalogGateway();
-    this.fareGateway = gatewayFactory.createFareGateway()
+    this.fareGateway = gatewayFactory.createFareGateway();
+    this.authGateway = gatewayFactory.createAuthGateway();
   }
 
-  async execute(input: Input): Promise<Order> {
+  async execute(input: Input): Promise<Output> {
+    const session = await this.authGateway.verify(input.token);
+    if (!session) throw new Error("Authentication failed");
     let order: Order
     if (!validate(input.cpf)) throw new Error('Invalid cpf');
     let sequence = await this.orderRepository.count()
     sequence++;
     const fromCEP = await this.fareGateway.getLocation(input.from.CEP)
     const toCEP = await this.fareGateway.getLocation(input.to.CEP)
-    order = new Order(input.cpf, input.id, fromCEP, toCEP, undefined, sequence)
+    order = new Order(input.cpf, input.id, fromCEP.CEP, toCEP.CEP, undefined, sequence)
     for (const item of input.items) {
       const product = await this.catalogGateway.getProduct(item.idProduct);
       order.addOrderItem(item.quantity, product);
@@ -49,8 +51,25 @@ export default class Checkout {
         order.addCoupon(coupon);
       }
     }
+    let items = [];
+    for (const item of order.getItems()) {
+      items.push({ idProduct: item.getIdProduct(), quantity: item.getQuantity() });
+    }
+    const fareData = { items: items, from: fromCEP.CEP, to: toCEP.CEP };
+    const totalFare = await this.fareGateway.simulateFare(fareData);
+    order.setTotalFare(totalFare)
     if (order.getItems().length > 0) await this.orderRepository.save(order);
-    return order;
+    const output = {
+      id: order.getId(),
+      cpf: order.getCpf(),
+      code: order.getCode(),
+      items: items,
+      from: order.getFromCEP(),
+      to: order.getToCEP(),
+      total: order.getTotal(),
+      totalFare: order.getTotalFare()
+    }
+    return output;
   }
 
 }
@@ -61,5 +80,18 @@ type Input = {
   items: { idProduct: number, quantity: number }[],
   coupon?: string,
   from: { CEP: string, latitude: number, longitude: number },
-  to: { CEP: string, latitude: number, longitude: number }
+  to: { CEP: string, latitude: number, longitude: number },
+  token: string
+}
+
+type Output = {
+  id: string,
+  cpf: string,
+  code: string,
+  items: { idProduct: number, quantity: number }[],
+  coupon?: string,
+  from: string,
+  to: string,
+  total: number,
+  totalFare: number
 }
--- backend/checkout/src/application/usecase/GetOrder.ts
+++ backend/checkout/src/application/usecase/GetOrder.ts
@@ -1,19 +1,53 @@
 import Order from "../../domain/entity/Order";
+import GatewayFactory from "../factory/GatewayFactory";
 import RepositoryFactory from "../factory/RepositoryFactory";
+import FareGateway from "../gateway/FareGateway";
 import OrderRepository from "../repository/OrderRepository";
 
 export default class GetOrder {
   orderRepository: OrderRepository
+  fareGateway: FareGateway;
 
   constructor(
-    repositoryFactory: RepositoryFactory
+    repositoryFactory: RepositoryFactory,
+    gatewayFactory: GatewayFactory
   ) {
     this.orderRepository = repositoryFactory.createOrderRepository();
+    this.fareGateway = gatewayFactory.createFareGateway();
   }
 
-  async execute(uuid: string): Promise<Order> {
+  async execute(uuid: string): Promise<Output> {
     const orderData = await this.orderRepository.get(uuid);
-    return orderData;
+    let items = [];
+    for (const item of orderData.getItems()) {
+      items.push({ idProduct: item.getIdProduct(), quantity: item.getQuantity() });
+    }
+    const fareData = { items: items, from: orderData.getFromCEP(), to: orderData.getToCEP() }
+    const totalFare = await this.fareGateway.simulateFare(fareData);
+    orderData.setTotalFare(totalFare)
+    const output = {
+      id: orderData.getId(),
+      cpf: orderData.getCpf(),
+      code: orderData.getCode(),
+      items: items,
+      from: orderData.getFromCEP(),
+      to: orderData.getToCEP(),
+      total: orderData.getTotal(),
+      totalFare: orderData.getTotalFare()
+    }
+    return output;
   }
+}
+
+type Output = {
+  id: string,
+  cpf: string,
+  code: string,
+  items: { idProduct: number, quantity: number }[],
+  coupon?: string,
+  from: string,
+  to: string,
+  total: number,
+  totalFare: number
 }
 
