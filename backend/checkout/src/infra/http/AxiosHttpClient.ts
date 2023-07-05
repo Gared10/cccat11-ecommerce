72d89e831e11038ea051eef6a322ac5a5d431fee
Gabriel Ângelo <gabriel.angelo14@gmail.com>
2023-07-04 23:39:59 -0300

    fix: enviando dados no body de uma requisição com método get

--- backend/checkout/src/infra/http/AxiosHttpClient.ts
+++ backend/checkout/src/infra/http/AxiosHttpClient.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import HttpClient from "./HttpClient";
 
 export default class AxiosHttpClient implements HttpClient {
-  async get(url: string): Promise<any> {
-    const response = await axios.get(url);
+  async get(url: string, data?: any): Promise<any> {
+    const response = await axios.get(url, { data });
     return response.data;
   }
   async post(url: string, data: any): Promise<any> {
